3e6fa210129bae5da9c3a7456ff05f353a698478
Pablo <p.garay@yahoo.com.ar>
2022-02-11 08:59:46 -0300

    Comienzo del proyecto

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>MVC</title>
+	<link rel="stylesheet" type="text/css" href="assets/css/estilos.css">
+</head>
+<body>
+
+	<h1>Modelo Vista Controlador</h1>
+
+</body>
+</html>
